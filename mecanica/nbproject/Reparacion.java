5202aec2b5a53c1bdfda42e33384cba7213e037f
CarlosGuaman7 <csguaman7@utpl.edu.ec>
2026-06-10 16:22:05 -0500

    second commit

--- mecanica/nbproject/Reparacion.java
+++ mecanica/nbproject/Reparacion.java
@@ -2,6 +2,40 @@
 package mecanica;
 
 
-public class Reparacion {
-    
+public class Reparacion extends Trabajo{
+    protected double precioMaterial;
+    protected String estado;
+    protected String material;
+
+     public Raparaciones(double precioMaterial, String estado, String material) {
+        super(idTrabajo,descripcion,horas,plazo);
+        this.precioMaterial = precioMaterial;
+        this.estado = estado;
+        this.material = material;
+    }
+
+    public double getPrecioMaterial() {
+        return precioMaterial;
+    }
+
+    public void setPrecioMaterial(double precioMaterial) {
+        this.precioMaterial = precioMaterial;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;12
+    }
 }
+
