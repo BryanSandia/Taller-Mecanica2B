02cd1c5ca9cd3748f5b3e248a0993201edee7cf4
CarlosGuaman7 <csguaman7@utpl.edu.ec>
2026-06-10 16:22:50 -0500

    three commit
    Added constructor and calcularPrecioRevision method.

--- mecanica/nbproject/Revision.java
+++ mecanica/nbproject/Revision.java
@@ -3,5 +3,15 @@ package mecanica;
 
 
 public class Revision {
+  
+    public Revision() {
+        super(idTrabajo,descripcion,horas,plazo);
+    }
+    
+    public double calcularPrecioRevision(){
+        double precio;
+        precio = super.cobrarprecio();
+        return precio + 20;
+    }
     
 }
